08c350036a9cc888c07e511ba8a7d72bf6e9a20c
Xander17700 <racinespartagees@gmail.com>
2026-02-20 21:55:17 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,23 +1,28 @@
-const fs = require('fs');
 const Parser = require('rss-parser');
-const parser = new Parser();
+const fs = require('fs');
+
+const parser = new Parser({
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+  }
+});
 
 const feeds = [
-"https://www.filae.com/ressources/blog/feed",
-"http://www.francegenweb.org/blog/index.php?feed=rss2",
-"https://www.geneatique.com/blog/feed",
-"https://home.heredis.com/feed",
-"https://blog.myheritage.fr/feed/",
-"https://www.genefede.eu/feed",
-"https://geneafinder.com/rss/rss.xml",
-"https://www.genealogiepratique.fr/feed",
-"https://genealogie-magazine.over-blog.com/rss",
-"https://www.geneanet.org/blog/feed",
-"https://geneatech.fr/feed",
-"https://www.histoire-genealogie.com/spip.php?page=backend",
-"https://www.rfgenealogie.com/rss.xml",
-"https://www.quotidien-genealogie.fr/feed",
-"https://www.piaf-archives.org/taxonomy/term/6/feed"
+  "https://www.filae.com/ressources/blog/feed",
+  "http://www.francegenweb.org/blog/index.php?feed=rss2",
+  "https://www.geneatique.com/blog/feed",
+  "https://home.heredis.com/feed",
+  "https://blog.myheritage.fr/feed/",
+  "https://www.genefede.eu/feed",
+  "https://geneafinder.com/rss/rss.xml",
+  "https://www.genealogiepratique.fr/feed",
+  "https://genealogie-magazine.over-blog.com/rss",
+  "https://www.geneanet.org/blog/feed",
+  "https://geneatech.fr/feed",
+  "https://www.histoire-genealogie.com/spip.php?page=backend",
+  "https://www.rfgenealogie.com/rss.xml",
+  "https://www.quotidien-genealogie.fr/feed",
+  "https://www.piaf-archives.org/taxonomy/term/6/feed"
 ];
 
 (async () => {
@@ -25,21 +30,20 @@ const feeds = [
 
   for (let url of feeds) {
     try {
-      let feed = await parser.parseURL(url);
-      feed.items.forEach(item => {
-        allItems.push({
-          title: item.title,
-          link: item.link,
-          pubDate: item.pubDate || item.isoDate,
-          source: feed.title
-        });
-      });
-    } catch (e) {
+      console.log("Lecture :", url);
+      const feed = await parser.parseURL(url);
+      allItems = allItems.concat(feed.items.map(item => ({
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate
+      })));
+    } catch (err) {
       console.log("Erreur :", url);
     }
   }
 
-  allItems.sort((a,b)=> new Date(b.pubDate) - new Date(a.pubDate));
+  allItems.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+  allItems = allItems.slice(0, 50);
 
-  fs.writeFileSync('feed.json', JSON.stringify(allItems.slice(0,200), null, 2));
+  fs.writeFileSync('feed.json', JSON.stringify(allItems, null, 2));
 })();
